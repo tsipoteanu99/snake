a4cdce0517646353aec4b2909bb4b510115f772a
tsipoteanu99 <61684810+tsipoteanu99@users.noreply.github.com>
2021-01-13 01:50:13 +0200

    finalul sesiunii + functionalitati complete

--- snake.py
+++ snake.py
@@ -61,6 +61,7 @@ FPS = 10
 block_size = 20
 best_score = 0
 font = pygame.font.SysFont('arial', 25)
+clicked = False
 
 
 # -------------------------------------------- IMPLEMENTARE GUI JOC -------------------------------------------------
@@ -140,7 +141,20 @@ class Snake:
         if self.check_if_final_state():
             game_over = True
 
-        return game_over
+        if game_over:
+
+            if PlayAgain(self.score, self.BEST).play_again():
+                game_over = False
+                self.direction = Direction.RIGHT
+                self.head = Coord(self.w / 2, self.h / 2)
+                self.tail = [self.head,
+                             Coord(self.head.x - block_size, self.head.y),
+                             Coord(self.head.x - (2 * block_size), self.head.y)]
+                self.score = 0
+                self.food = None
+                self.randomize_food()
+
+        return game_over, self.BEST
 
     def randomize_food(self):
         """
@@ -152,7 +166,7 @@ class Snake:
         x = random.randint(0, (self.w - block_size) // block_size) * block_size
         y = random.randint(0, (self.h - block_size) // block_size) * block_size
         self.food = Coord(x, y)
-        if self.food in self.tail:
+        if self.food in self.tail or self.food in obstacles:
             self.randomize_food()
 
     def render(self):
@@ -249,11 +263,125 @@ class Snake:
             pygame.draw.rect(self.display, BLACK, pygame.Rect(obstacle[0], obstacle[1], block_size, block_size))
 
 
+def display_best_score():
+    """
+    Aceasta functie va afisa fereastra finala, cand jucatorul decide sa paraseasca jocul si va apasa "quit game". In aceasta fereastra va fi afisat cel
+        mai mare scor. Pentru a iesi de tot din joc va mai trebui apasata pentru ultima oara o tasta la intamplare.
+    :return: No return.
+    """
+    while True:
+        display = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Snake')
+        display.fill(background)
+        text = font.render("Congrats! Your Best Score is: " + str(best_score), True, GREEN)
+        text2 = font.render("Press any key to exit the game", True, GREEN)
+        display.blit(text, [width / 4 + 20, height / 2 - 50])
+        display.blit(text2, [width / 3 - 40, height - 100])
+        pygame.display.flip()
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+            pygame.quit()
+            quit()
+
+
+class PlayAgain:
+    play = False
+    exit = False
+
+    def __init__(self, final_score, best_scoree, w=width, h=height):
+        """
+        In constructor vom prelua datele din momentul in care jocul s-a terminat si le vom folosi pentru a afisa fereastra cu play again unde se afla
+            scorul din meciul respectiv, cel mai mare scor din sesiunea de joc dar si doua butoane, unul pentru "Play again", altul pentru "Quit Game".
+        :param final_score: reprezinta scorul final din meciul abia terminat
+        :param best_scoree: reprezinta best score-ul din sesiunea actuala de joc
+        :param w: reprezinta latimea ferestrei de joc
+        :param h: reprezinta inaltimea ferestrei de joc
+        """
+        self.w = w
+        self.h = h
+        self.score = final_score
+        self.best_score = best_scoree
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+
+    def play_again(self):
+        """
+        Aceasta functie este responsabila cu randarea ferestrei de final de joc, unde sunt afisate scorurile si butoanele care ofera posibilitatea
+            de a juca din nou sau de a parasi sesiunea de joc curenta.
+        :return:
+        """
+        while self.play is False:
+            self.display.fill(background)
+            text = font.render("Final Score: " + str(self.score), True, GREEN)
+            text2 = font.render("Best Score: " + str(self.best_score), True, GREEN)
+            self.display.blit(text, [self.w / 2 - 77, self.h / 4])
+            self.display.blit(text2, [self.w / 2 - 75, self.h / 6])
+            play_again = Button(self.w / 2 - 90, self.h / 3, "Play Again", self.display)
+            if play_again.draw_button():
+                self.play = True
+            quit_game = Button(self.w / 2 - 90, self.h / 2 + 20, "Quit Game", self.display)
+            quit_game.draw_button()
+            pygame.display.update()
+            event = pygame.event.poll()
+            if quit_game.draw_button():
+                display_best_score()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        return self.play
+
+
+class Button:
+    width = 180
+    height = 40
+
+    def __init__(self, x, y, text, display):
+        """
+        In constructor vom primi date care ne vor ajuta sa construim un buton.
+        :param x: coordonata x a butonului
+        :param y: coordonata y a butonului
+        :param text: textul afisat pe buton
+        :param display: display-ul pygame peste care va fi afisat butonul
+        """
+        self.x = x
+        self.y = y
+        self.text = text
+        self.display = display
+
+    def draw_button(self):
+        """
+        Aceasta functie va folosi resursele primite in constructor pentru a esena un buton care va detecta daca a fost apasat si
+            va schimba variabila "action" in acest caz.
+        :return: Returneaza variabila action, care indica daca butonul a fost apasat sau nu.
+        """
+        global clicked
+        triggered = False
+        pozitie = pygame.mouse.get_pos()
+
+        button_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        if button_rect.collidepoint(pozitie):
+            if pygame.mouse.get_pressed(3)[0] == 1:
+                clicked = True
+                pygame.draw.rect(self.display, RED, button_rect)
+            elif pygame.mouse.get_pressed(3)[0] == 0 and clicked is True:
+                clicked = False
+                triggered = True
+            else:
+                pygame.draw.rect(self.display, (255, 255, 0), button_rect)
+        else:
+            pygame.draw.rect(self.display, GREEN, button_rect)
+
+        text_img = font.render(self.text, True, BLACK)
+        text_len = text_img.get_width()
+        self.display.blit(text_img, (self.x + int(self.width / 2) - int(text_len / 2), self.y + 5))
+        return triggered
+
+
 if __name__ == '__main__':
     snake = Snake()
 
     while True:
-        end_game = snake.start_game()
+        end_game, best_score = snake.start_game()
         if end_game:
             break
 
